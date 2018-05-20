6968b919e53de82f02550a5017b415f2e4fc21fc
earl <johnearlagbay@gmail.com>
2018-05-20 23:56:33 +0800

    logging

--- service-spring-rest/src/main/java/com/hello/GreetingController.java
+++ service-spring-rest/src/main/java/com/hello/GreetingController.java
@@ -1,6 +1,9 @@
-package hello;
+package com.hello;
 
 import java.util.concurrent.atomic.AtomicLong;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,8 +13,15 @@ public class GreetingController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
+    private static final Logger logger = LogManager.getLogger(GreetingController.class.getName());
+
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        logger.debug("Debugging log");
+        logger.info("Info log");
+        logger.warn("Hey, This is a warning!");
+        logger.error("Oops! We have an Error. OK");
+        logger.fatal("Damn! Fatal error. Please fix me.");
         return new Greeting(counter.incrementAndGet(),
                 String.format(template, name));
     }
